ffd86df90867bd287c23a3464ff933aa9e3648bf
Simon Frost <sdwfrost@gmail.com>
2015-10-11 16:54:28 +0100

    Changed example

--- examples/sir.jl
+++ examples/sir.jl
@@ -1,5 +1,4 @@
 using Gillespie
-using Gadfly
 
 function F(x,parms)
   (S,I,R) = x
@@ -15,8 +14,8 @@ parms = [0.1/1000.0,0.01]
 tf = 250.0
 srand(1234)
 
-result = ssa(x0,F,nu,parms,tf)
-@time result = ssa(x0,F,nu,parms,tf)
+#result = ssa(x0,F,nu,parms,tf)
+#@time result = ssa(x0,F,nu,parms,tf)
 
 type SIRFunction
 end
@@ -24,16 +23,3 @@ call(::Type{SIRFunction},x,parms) = F(x,parms)
 
 result2 = ssa(x0,SIRFunction,nu,parms,tf)
 @time result2 = ssa(x0,SIRFunction,nu,parms,tf)
-
-data = ssa_data(result)
-
-p=plot(data,
-  layer(x="time",y="x1",Geom.step,Theme(default_color=color("red"))),
-  layer(x="time",y="x2",Geom.step,Theme(default_color=color("blue"))),
-  layer(x="time",y="x3",Geom.step,Theme(default_color=color("green"))),
-  Guide.xlabel("Time"),
-  Guide.ylabel("Number"),
-  Guide.manual_color_key("Population",
-                            ["S", "I", "R"],
-                            ["red", "blue", "green"]),
-  Guide.title("SIR epidemiological model"))
